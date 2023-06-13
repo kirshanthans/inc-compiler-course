0de88d0d4bc2114b2936355d1e7a84139e4d75c2
Kirshanthan Sundararajah <ksundar@purdue.edu>
2023-06-13 18:38:25 -0400

    fixing path

--- run-tests.py
+++ run-tests.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-sys.path.append('../python-student-support-code')
-sys.path.append('../python-student-support-code/interp_x86')
+sys.path.append('../inc-compiler-course')
+sys.path.append('../inc-compiler-course/interp_x86')
 
 import compiler
 import interp_Lvar
